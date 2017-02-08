bad8e0092f8dbff518a3d8491f7f0b524c74cd99
Brent Brewington <brent.brewington@gmail.com>
2017-02-07 21:14:07 -0500

    Update Atlanta Scraper, Data

--- atl/AtlantaCrime.R
+++ atl/AtlantaCrime.R
@@ -1,35 +1,35 @@
 library(dplyr); library(rvest); library(stringr); library(lubridate);
 library(readr); library(httr); library(readxl)
 
-get_apd_crime_data <- function(write_files = TRUE){
+get_atlanta_crime_data <- function(write_files = TRUE){
      if(!dir.exists("Data")){
           dir.create("Data")
      }
      
-     crime_links_html <- 
+     atlanta_crime_links_html <- 
           read_html("http://www.atlantapd.org/i-want-to/crime-data-downloads") %>% 
           html_nodes("#widget_362_0_197 .content_link")
      
-     crime_links <- 
-          data_frame(filename = crime_links_html %>% html_text(),
-                     url = crime_links_html %>% html_attr("href") %>% paste0("http://www.atlantapd.org", .))
+     atlanta_crime_links <- 
+          data_frame(filename = atlanta_crime_links_html %>% html_text(),
+                     url = atlanta_crime_links_html %>% html_attr("href") %>% paste0("http://www.atlantapd.org", .))
      
      temp_list <- vector(mode = "list", length = 9)
-     for(i in seq_along(crime_links$url)){
-          temp_list[[i]] <- headers(GET(crime_links$url[i]))     
+     for(i in seq_along(atlanta_crime_links$url)){
+          temp_list[[i]] <- headers(GET(atlanta_crime_links$url[i]))     
      }
      
-     crime_links_fileinfo <- 
+     atlanta_crime_links_fileinfo <- 
           lapply(temp_list, function(x) data_frame(type = x$`content-type`,
                                                    disposition = x$`content-disposition`, 
                                                    documenttitle = x$`content-documenttitle`,
                                                    lastmodified = x$`last-modified`)) %>%
           bind_rows()
      
-     crime_links <- bind_cols(crime_links, crime_links_fileinfo) %>% 
+     atlanta_crime_links <- bind_cols(atlanta_crime_links, atlanta_crime_links_fileinfo) %>% 
           mutate(filename2 = str_sub(disposition, start = 11, end = -2))
      
-     crime_links
+     atlanta_crime_links
      
      get_crime_zipfile <- function(zip.url){
           td <- tempdir()
@@ -50,23 +50,23 @@ get_apd_crime_data <- function(write_files = TRUE){
           return(read_excel(file.name, sheet="Query"))
      }
      
-     crime_data <- vector(mode = "list", length = nrow(crime_links))
-     for(i in seq_along(crime_links$filename)){
-          if(crime_links$type[i] == "application/zip"){
-               crime_data[[i]] <- get_crime_zipfile(crime_links$url[i])
-          }else if(crime_links$type[i] == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"){
-               crime_data[[i]] <- 
-                    get_crime_nonzipfile(crime_links$url[i], file.name = crime_links$filename2[i])
+     atlanta_crime_data <- vector(mode = "list", length = nrow(atlanta_crime_links))
+     for(i in seq_along(atlanta_crime_links$filename)){
+          if(atlanta_crime_links$type[i] == "application/zip"){
+               atlanta_crime_data[[i]] <- get_crime_zipfile(atlanta_crime_links$url[i])
+          }else if(atlanta_crime_links$type[i] == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"){
+               atlanta_crime_data[[i]] <- 
+                    get_crime_nonzipfile(atlanta_crime_links$url[i], file.name = atlanta_crime_links$filename2[i])
           }
      }
      
-     xlsx_filenames <- crime_links %>% .[str_detect(.$filename2, "\\.xlsx"),] %>% .$filename2
+     xlsx_filenames <- atlanta_crime_links %>% .[str_detect(.$filename2, "\\.xlsx"),] %>% .$filename2
      for(i in seq_along(xlsx_filenames)){
           unlink(xlsx_filenames[i])
      }
      
-     crime_data2 <- 
-          bind_rows(crime_data) %>%
+     atlanta_crime_data2 <- 
+          bind_rows(atlanta_crime_data) %>%
           mutate(rpt_date = mdy(rpt_date), 
                  occur_datetime = mdy_hms(paste(occur_date, occur_time)), occur_date = mdy(occur_date),
                  poss_datetime = mdy_hms(paste(poss_date, poss_time)), poss_date = mdy(poss_date),
@@ -74,17 +74,14 @@ get_apd_crime_data <- function(write_files = TRUE){
                  x = round(as.numeric(x), 5), y = round(as.numeric(y), 5))
      
      if(write_files){
-          print("Writing Data/crime_data.rds")
-          write_rds(crime_data2, "Data/crime_data.rds")
+          print("Writing atl/AtlantaCrime.csv")
+          write_csv(atlanta_crime_data2, "atl/AtlantaCrime.csv")
           
-          print("WritingData/crime_data.csv")
-          write_csv(crime_data2, "Data/crime_data.csv")
-          
-          print("Writing Data/crime_links.csv")
-          write_csv(crime_links, "Data/crime_links.csv")
+          print("Writing atl/AtlantaCrime_Links.csv")
+          write_csv(atlanta_crime_links, "atl/AtlantaCrime_Links.csv")
      }
      
-     return(crime_data2)
+     return(atlanta_crime_data2)
 }
 
-crime_data <- get_apd_crime_data(write_files = TRUE)
+atlanta_crime_data <- get_atlanta_crime_data(write_files = TRUE)
