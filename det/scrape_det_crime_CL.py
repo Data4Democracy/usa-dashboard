69620602c4606f3766ad6bd669de9b67d5c663c0
Tyler Zupan <jtzupan@gmail.com>
2017-03-21 22:15:14 -0400

    finished basic scraper and began to work on version with command line args

---
+++ det/scrape_det_crime_CL.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 21 22:12:21 2017
+
+@author: johnzupan
+"""
+
+
+
+import arrow
+import pandas as pd
+import argparse
+
+
+def extract_crime_data(LIMIT_PER_REQ=10000):
+    # Note: this can be set to entire dataset but request may timeout on slow connection    
+    
+    # Const
+    BASE_URL = "https://data.detroitmi.gov/resource/i9ph-uyrp.json?$order=crimeid%20asc"
+     
+    
+    
+    # Setup
+    available = True
+    offset = 0
+    df = pd.DataFrame()
+    
+    # Pull everything available
+    while available :
+        print('\rEntries pulled: {} '.format(len( df.index)))
+        if year:
+            pageUrl = BASE_URL + '&$limit=' + str(LIMIT_PER_REQ) + '&$offset=' + str(offset)
+        else:    
+            pageUrl = BASE_URL + '&$limit=' + str(LIMIT_PER_REQ) + '&$offset=' + str(offset)
+        dfTemp = pd.read_json( pageUrl )
+        df = df.append(dfTemp, ignore_index=True)
+        available = len(dfTemp.index) > 0
+        offset += LIMIT_PER_REQ
+    print()
+    
+    # Dedicated year, month, day fields
+    df['year'] = df['incidentdate'].map(lambda x : arrow.get(x).datetime.year)
+    df['month'] = df['incidentdate'].map(lambda x : arrow.get(x).datetime.month)
+    df['day'] = df['incidentdate'].map(lambda x : arrow.get(x).datetime.day)
+    
+    # Get counts based on columns of interest
+    df['count'] = 0
+    df = df.groupby(['year', 'month', 'day', 'category']).agg({'count' : 'count'})
+    df = df.reset_index()
+    df = df.loc[:, ['year', 'month', 'day', 'category', 'count']]
+  
+    
+    # Write out to csv by year
+    yearMax = df.loc[:, 'year'].max()
+    yearMin = df.loc[:, 'year'].min()
+    for yr in range(yearMin, yearMax + 1) :
+        dfAnnual = df[df['year'] == yr]
+        fileName = 'data/det-{}-crime.csv'.format(yr)
+        dfAnnual.to_csv( fileName, index=False)
+        print( 'Wrote {} entries to'.format(len(dfAnnual.index )), fileName)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('limit', help='how many records would you like to extract \
+                        at a time?', type=int)
+    
+    args = parser.parse_args()
+    result = extract_crime_data(LIMIT_PER_REQ=args.limit)
+    print result
+    
+if __name__ == '__main__':
+    main()
+    
+    
+    
+    
