dd30f1b862f994677279a7334fca7b26c94f1bf7
kraemerd17 <kraemerd17@grinnell.edu>
2017-05-24 22:51:09 -0500

    Incorporated the bls library into the project
    The repository https://github.com/OliverSherouse/bls is on the PyPI, and it does essentially all that I need at the moment (I've submitted a pull request to the repository that extends its functionality, but it isn't critical). This update uses the bls package to acquire and store the BLS data, and updates the bls query script correspondingly.

--- scrapers/src/unrate/scripts/bls_query_to_csv.py
+++ scrapers/src/unrate/scripts/bls_query_to_csv.py
@@ -4,9 +4,8 @@ Usage:
     yyyy --end yyyy
 """
 import argparse
-import requests
-import json
 from datetime import datetime as dt
+from bls import api
 
 parser = argparse.ArgumentParser()
 
@@ -34,40 +33,25 @@ parser.add_argument(
         )
 
 parser.add_argument(
-        "fetch", 
-        help="fetch data sets specified at the command line by series IDs from BLS",
+        "series",
+        help=("fetch data sets specified at the command line by series IDs "
+              "from BLS"),
         nargs='+',
         type=str
         )
 
-parser.add_argument(
-        "--delimiter", "-d",
-        help="specify csv delimiter",
-        type=str,
-        default=',',
-        )
-
 args = parser.parse_args()
 
-# For more info on this check out https://www.bls.gov/developers/api_python.htm
-headers = {'Content-type': 'application/json'}
-data = json.dumps({'seriesid': args.fetch, 'startyear': args.start, 'endyear':
-    args.end, 'registrationkey': args.key})
+data = api.get_series(series=args.series,
+                      startyear=args.start,
+                      endyear=args.end,
+                      key=args.key)
 
-p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/',
-        data=data, headers=headers)
-
-json_data = json.loads(p.text)
-
-for (i, series) in enumerate(json_data['Results']['series']):
-    # I'm assuming that the header for the first entry in the series is the
-    # header for every entry in the series. I don't know if that is bad.
-    header = list(series['data'][0].keys())
-    with open('../data/{}.csv'.format(series['seriesID']), 'w+') as f:
-        f.write(args.delimiter.join(header + ['\n']))
-        for item in series['data']:
-            f.write(args.delimiter.join([str(item[column]) for column in header] + ['\n']))
-
+data.to_csv('../data/national/{}.{} {}.csv'.format(','.join(args.series),
+                                                   args.start,
+                                                   args.end),
+            header=True,
+            index_label='date')
         
             
 
