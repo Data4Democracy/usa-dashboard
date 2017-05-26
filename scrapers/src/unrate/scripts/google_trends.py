e38eadb1d567886e4920d53ed53a06e80a3e0cc9
kraemerd17 <kraemerd17@grinnell.edu>
2017-05-26 13:50:30 -0500

    Command line arguments for the google_trends.py file
    The user can now specify a list of search terms to be requested by the script, as well as the start and end dates for the request.

--- scrapers/src/unrate/scripts/google_trends.py
+++ scrapers/src/unrate/scripts/google_trends.py
@@ -1,61 +1,81 @@
 """
-Usage:
-    $ python3 google_trends google_username google_password
+Usage: python google_trends.py [-h] [--start START] [--end END]
+                        user pswd terms [terms ...]
 """
 from pytrends.request import TrendReq
 from datetime import datetime as dt
-import pandas as pd
 import argparse
 
 # Argument parsing
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
-        "user",
-        help="Google account user name",
-        type=str
-        )
+    "user",
+    help="Google account user name",
+    type=str,
+    nargs=1
+)
 
 parser.add_argument(
-        "pswd",
-        help="Google account password",
-        type=str
-        )
+    "password",
+    help="Google account password",
+    type=str,
+    nargs=1
+)
+
+parser.add_argument(
+    "terms",
+    help="Desired search terms to be queried",
+    type=str,
+    nargs='+'
+)
+
+parser.add_argument(
+    "--start",
+    help="Start date, formatted YYYY-MM-DD",
+    type=str,
+    default=str(dt(dt.today().year - 1, 1, 1).date())
+)
+
+parser.add_argument(
+    "--end",
+    help="End date, formatted YYYY-MM-DD",
+    type=str,
+    default=str(dt.today().date())
+)
 
 args = parser.parse_args()
 
 # credentials
 username = args.user
-password = args.pswd
+password = args.password
 
 language = 'en-US'
 timezone = 360
 
-query_terms = ['jobs']
+query_terms = args.terms
 
 # time span for search
 # times = 'YYYY-MM-DD YYYY-MM-DD', where the first substring is the start 
 # date and the second substring is the end date
-start = str(dt(2016, 1, 1).date())
-end = str(dt.today().date())
-times = "{} {}".format(start, end)
+times = " ".join([args.start, args.end])
 
 # geographic details
-location='US'
+location = 'US'
 
 # establish connection
-query = TrendReq(username, 
-        password, 
-        hl=language, 
-        tz=timezone, 
-        custom_useragent=None)
+query = TrendReq(username,
+                 password,
+                 hl=language,
+                 tz=timezone,
+                 custom_useragent=None)
 
 # query setup
-query.build_payload(query_terms, 
-        cat=0, 
-        timeframe=times,
-        geo='US', 
-        gprop='')
+query.build_payload(query_terms,
+                    cat=0,
+                    timeframe=times,
+                    geo='US',
+                    gprop='')
 
 # request to pandas table
 data = query.interest_over_time()
